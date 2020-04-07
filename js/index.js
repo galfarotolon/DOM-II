58135f43d16feba8ed2680900cc6cf0916e28c35
Guillermo Alfaro <galfarotolon@gmail.com>
2020-04-07 15:37:30 -0500

    Header img with 2 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+//Header img 
+
+const headerImg = document.querySelector("header img")
+
+
+headerImg.addEventListener("mouseenter", function () {
+
+    headerImg.src = "img/fun-bus-blackwhite.jpg"
+});
+
+headerImg.addEventListener("mouseleave", function () {
+
+    headerImg.src = "img/fun-bus.jpg"
+});
